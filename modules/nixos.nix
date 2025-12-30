3d482ff8bf65473f527127be0fa7dbea4eeb85db
Nikita <bananad3v@noreply.codeberg.org>
2025-12-30 20:55:18 +0300

    feat(modules/nixos): Merge attrs instead to not evaluate disabled code

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -6,15 +6,16 @@
 }:
 let
   cfg = config.programs.niri;
+  inherit (lib) mkIf mkMerge mkEnableOption;
 in
 {
   options.programs.niri = {
-    withUWSM = lib.mkEnableOption "UWSM support" // {
+    withUWSM = mkEnableOption "UWSM support" // {
       description = ''
         Launch Niri with the Universal Wayland Session Manager. This has better systemd support and automatically starts `graphical-session.target` and `wayland-session@niri.target`.
       '';
     };
-    withXDG = lib.mkEnableOption "XDG portal support" // {
+    withXDG = mkEnableOption "XDG portal support" // {
       description = ''
         Enable XDG portal support for Niri.
       '';
@@ -22,26 +23,29 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    programs.uwsm = lib.mkIf cfg.withUWSM {
-      enable = true;
-      waylandCompositors = {
-        hyprland = {
-          prettyName = "Niri";
-          comment = "A scrollable-tiling Wayland compositor";
-          binPath = "/run/current-system/sw/bin/niri-session";
+  config = mkIf cfg.enable mkMerge [
+    (mkIf cfg.withUWSM {
+      programs.uwsm = {
+        enable = true;
+        waylandCompositors = {
+          hyprland = {
+            prettyName = "Niri";
+            comment = "A scrollable-tiling Wayland compositor";
+            binPath = "/run/current-system/sw/bin/niri-session";
+          };
         };
       };
-    };
-
-    xdg.portal = lib.mkIf cfg.withXDG {
-      enable = true;
-      xdgOpenUsePortal = lib.mkDefault true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gnome
-        xdg-desktop-portal-gtk
-      ];
-      configPackages = [ cfg.package ];
-    };
-  };
+    })
+    (mkIf cfg.withXDG {
+      xdg.portal = {
+        enable = true;
+        xdgOpenUsePortal = lib.mkDefault true;
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gnome
+          xdg-desktop-portal-gtk
+        ];
+        configPackages = [ cfg.package ];
+      };
+    })
+  ];
 }
