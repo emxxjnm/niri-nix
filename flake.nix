4f160bec10cec459f873e9c08413ddf7b396be98
poz <poz@poz.pet>
2026-04-29 12:15:09 +0200

    escape shell substitution in the git hook

--- flake.nix
+++ flake.nix
@@ -40,8 +40,8 @@
                     cat > .git/hooks/pre-commit<< EOF
                     #!/usr/bin/env bash
 
-                    cat "$(nix build .#docs-nixos --print-out-paths --no-link)" > ./nixos-options.md
-                    cat "$(nix build .#docs-home --print-out-paths --no-link)" > ./home-options.md
+                    cat "\$(nix build .#docs-nixos --print-out-paths --no-link)" > ./nixos-options.md
+                    cat "\$(nix build .#docs-home --print-out-paths --no-link)" > ./home-options.md
 
                     git add ./nixos-options.md
                     git add ./home-options.md
